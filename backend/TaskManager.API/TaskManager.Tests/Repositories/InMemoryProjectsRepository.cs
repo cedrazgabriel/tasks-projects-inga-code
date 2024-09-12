4d92525f489fdd16cc83d7d6ce63e82dee36992c
Gabriel Cedraz <cedrazdev@gmail.com>
2024-09-12 12:46:29 -0300

    adiciona primeiro teste unitario

---
+++ backend/TaskManager.API/TaskManager.Tests/Repositories/InMemoryProjectsRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Application.Repositories.Contracts;
+using TaskManager.API.DTO.Response;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Tests.Repositories
+{
+    public class InMemoryProjectsRepository : IProjectRepository
+    {
+        public List<Project> projects;
+
+        public InMemoryProjectsRepository()
+        {
+            projects = new List<Project>();
+        }
+
+        public Task CreateAsync(Project project)
+        {
+            projects.Add(project);
+            return Task.CompletedTask;
+        }
+
+        public Task<PaginatedResult<Project>> GetPaginatedProjectsAsync(int page, int pageSize)
+        {
+            var paginatedProjects = projects
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var result = new PaginatedResult<Project>
+            {
+                Items = paginatedProjects,
+                Page = page,
+                PageSize = pageSize,
+                TotalRecords = projects.Count
+            };
+
+            return Task.FromResult(result);
+        }
+
+        public Task<Project> GetProjectByIdAsync(Guid id)
+        {
+            var project = projects.FirstOrDefault(p => p.Id == id);
+            return Task.FromResult(project);
+        }
+
+        public Task UpdateAsync(Project project)
+        {
+            var existingProject = projects.FirstOrDefault(p => p.Id == project.Id);
+            if (existingProject != null)
+            {
+                projects.Remove(existingProject);
+                projects.Add(project);
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Project project)
+        {
+            projects.Remove(project);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ backend/TaskManager.API/TaskManager.Tests/UseCases/Projects/CreateProjectUseCaseTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Threading.Tasks;
+using TaskManager.Application.Repositories.Contracts;
+using TaskManager.Application.Services;
+using TaskManager.Application.UseCases.Projects;
+using TaskManager.Domain.Entities;
+using TaskManager.Tests.Repositories;
+using Xunit;
+
+namespace TaskManager.Tests.UseCases.Projects
+{
+    public class CreateProjectUseCaseTests
+    {
+        [Fact]
+        public async Task Execute_ShouldCreateProjectAndStoreInRepositoryAndClearCache()
+        {
+            // Arrange
+            var inMemoryProjectRepository = new InMemoryProjectsRepository();
+            var mockCacheService = new Mock<ICacheService>();
+
+            var useCase = new CreateProjectUseCase(inMemoryProjectRepository, mockCacheService.Object);
+            var projectName = "Test Project";
+
+            // Act
+            var createdProject = await useCase.Execute(projectName);
+
+            // Assert
+           
+            Assert.Equal(inMemoryProjectRepository.projects?.Count() , 1); 
+            Assert.Equal(projectName, createdProject.Name); 
+
+            // Verifica se o cache foi invalidado
+            mockCacheService.Verify(cache => cache.RemoveByPrefixAsync("projects_cache"), Times.Once);
+        }
+    }
+}
